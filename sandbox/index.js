4c85a3c78b49f428ea29ef044030700f8ce6510c
Anissoft <anissoft@yandex-team.com>
2022-11-05 23:15:40 +0700

    fixed multiple values in header

--- sandbox/index.js
+++ sandbox/index.js
@@ -30,24 +30,29 @@ http.createServer( function (req, res) {
   });
 }).listen(9000, (...srgs) => console.log('started http server', ...srgs));
 
-function postGzip() {
-  exec(`curl -v -i -k https://localhost:8000/?param=${Date.now()} -H'Content-Type: application/json' -H'Content-Encoding: gzip' --data-binary @${path.resolve(__dirname, 'body.gz')}`);
+function postGzipLocally() {
+  exec(`curl -v -i -k https://localhost:8000/root/?param=${Date.now()} -H'Content-Type: application/json' -H'Content-Encoding: gzip' --data-binary @${path.resolve(__dirname, 'body.gz')}`);
 }
 
-function getJson() {
-  axios.get('http://www.boredapi.com/api/activity');
+function postJsonLocally() {
+  axios.post(`http://localhost:9000/api/v1/method?param=${Date.now()}`, { key: 'value' });
 }
 
-function postJson() {
-  axios.post(`http://localhost:9000/?param=${Date.now()}`, { key: 'value' });
-}
+const urls = [
+  'https://api.publicapis.org/entries',
+  'https://catfact.ninja/fact',
+  'https://api.coindesk.com/v1/bpi/currentprice.json',
+  'https://www.boredapi.com/api/activity',
+  'https://dog.ceo/api/breeds/image/random',
+  'https://official-joke-api.appspot.com/random_joke',
+  'https://randomuser.me/api/',
+]
 
-setTimeout(postGzip, 1000);
-setTimeout(postJson, 1000);
-setTimeout(getJson, 2000);
-setTimeout(postJson, 3000);
+setInterval(postGzipLocally, 6000);
+setInterval(postJsonLocally, 10000);
 
-setInterval(postGzip, 10000);
-setInterval(getJson, 15000);
-setInterval(postJson, 20000);
-// 
+let i = 0;
+setInterval(() => {
+  axios.get(urls[i]);
+  i = i >= urls.length - 1 ? 0 : i + 1;
+}, 3000);
