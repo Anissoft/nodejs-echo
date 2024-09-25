5ce3310d0d7ad636958e0e13051900c0e286ba8a
Aleksey Anisimov <anissoftkun@gmail.com>
2024-09-25 04:11:26 +0000

    fix: filter out unusable chunks

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -5,7 +5,7 @@ export const parseBodyFromChunks = (
   contentEncoding?: string,
   encoding: BufferEncoding = 'base64'
 ) => {
-  chunks = chunks?.filter(Boolean);
+  chunks = chunks?.filter(chunk => Buffer.isBuffer(chunks) || typeof chunk === 'string');
   if (chunks == null || chunks.length === 0) {
     return '';
   }
